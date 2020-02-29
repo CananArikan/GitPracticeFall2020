b027d3ff0ac15cb27d28ba5f27c60972f5ba6de6
CananArikan <arikancnn@gmail.com>
2020-02-29 15:25:07 -0500

    modified StringPractice class

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -5,6 +5,8 @@ public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Come on");
+        String str = "Java is fun";
+        System.out.println(str);
     }
 
 }
